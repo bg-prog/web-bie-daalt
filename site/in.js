a9b6ce441c84067dfac1b0a88d7565d7b6548ea1
bg-prog <begzsureng18@gmail.com>
2020-07-16 15:39:26 +0800

    web bie daalt

---
+++ site/in.js
@@ -0,0 +1,24 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+function openNav1() {
+  document.getElementById("mySidenav1").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav1() {
+  document.getElementById("mySidenav1").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+var fname = document.getElementById("fname").value;
+var lname = document.getElementById("lname").value;
+var username = document.getElementById("username").value;
+var mail = document.getElementById("mail").value;
+var password = document.getElementById("password").value;
+var confirm = document.getElementById("confirm").value;
